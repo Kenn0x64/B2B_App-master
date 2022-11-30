acea9aa9dd342b09d11404efe101def45bfe4bb1
Matt0x64 <darkspace470@gmail.com>
2022-11-30 07:45:37 +0300

    update

--- secondry_branch_app/android/app/src/main/kotlin/com/example/secondry_branch/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.secondry_branch
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ tokyoapp/android/app/src/main/kotlin/com/example/tokyoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tokyoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
